2c2d5dec42c36be5f74227019ce3ec436f7cdd99
João Soares <96133466+SilvaNeto29@users.noreply.github.com>
2022-08-09 09:37:43 -0300

    Add files via upload

---
+++ mvc/src/controllers/UsuariosController.php
@@ -0,0 +1,12 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class UsuariosController extends Controller {
+
+    public function add() {
+        $this->render('add');
+    }
+
+}
---
+++ mvc/src/views/pages/add.php
@@ -0,0 +1,5 @@
+<?php $render('header'); ?>
+
+<h2>Adicionar novo usuário</h2>
+
+<?php $render('footer'); ?>
